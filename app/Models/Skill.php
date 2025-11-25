bda94b361a2637a635de16ee164d1e65d7cf0a8a
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-11-24 22:09:05 -0500

    add scope to skills

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\Proficiency;
+use App\Models\Scopes\CurrentUserScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ScopedBy([CurrentUserScope::class])]
 class Skill extends Model
 {
     use HasFactory, SoftDeletes;
--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -3,12 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\Skill;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class SkillSeeder extends Seeder
 {
     public function run(): void
     {
-        Skill::factory(10)->create();
+        Skill::factory(10)->create([
+            'user_id' => User::where('email', 'test@example.com')->first()->id
+        ]);
     }
 }
